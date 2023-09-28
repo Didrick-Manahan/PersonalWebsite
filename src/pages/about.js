8427209df8b817b28fc446e5445caeaf1b1b9cd9
Didrick Manahan <didrick.manahan@gmail.com>
2023-09-27 23:04:40 -0400

    Experience/Education components

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,8 @@ import Image from "next/image";
 import profilePic from "../../public/images/profile/HeadshotExelon.PNG";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
 import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
 
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
@@ -105,6 +107,8 @@ const about = () => {
             </div>
           </div>
           <Skills />
+          <Experience />
+          <Education />
         </Layout>
       </main>
     </div>
